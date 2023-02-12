c8f0e855559fa4eff7270fc6a7939cd72d11d20c
Mohammad Hassaan Zareef <113226189+maaz-mohd@users.noreply.github.com>
2023-02-12 15:18:25 +0530

    Update hellow.sh

--- hellow.sh
+++ hellow.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-#
-echo "Hellow World!"
 
-cat << TEXT
-Now I can Add multiple lines of text here 
-it might be any number of lines
-either 3,4,5.. 
-TEXT
+echo "Hellow World!"
 
 echo "End"
