12a0836e6f7adf057f68d48cd54bb5dfb00960dd
Mohammad Hassaan Zareef <113226189+maaz-mohd@users.noreply.github.com>
2023-02-21 12:45:15 +0530

    user input & arguments in shell script

--- section00/2 input-arguments.sh
+++ section00/2 input-arguments.sh
@@ -4,3 +4,18 @@ name="Maaz_Mohd"
 echo "Hellow ${name}, please enter your age"
 read age
 echo "My age is ${age}"
+
+##########################################################
+
+# Arguments for Name and passwd
+
+#!/bin/bash
+
+note="welcome"
+echo "${note}, please enter your name"
+read -p "name: " name
+echo "Please enter your password"
+read -sp "password: " passwd
+echo
+echo "Your name is ${name}"
+echo "Your password is ${passwd}"
